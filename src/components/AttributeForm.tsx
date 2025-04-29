6ec318bb8b0bc8d3ead0604b935705a7491a49ac
KAChamika <tharuprocessing.chamika@gmail.com>
2025-04-29 16:05:44 +0530

    adding attribute form

--- src/components/AttributeForm.tsx
+++ src/components/AttributeForm.tsx
@@ -1,15 +1,39 @@
-import { Row, Col, Typography, Form, Input, Button } from 'antd';
+import { Row, Col, Typography, Form, Input, Button, message } from 'antd';
 import { SaveOutlined } from '@ant-design/icons';
+
+import { IAttributeValue } from '../types/attributeValue.interface';
 
 const { Title } = Typography;
 const FormItem = Form.Item;
 
 const AttributeForm = () => {
+    const [displayMessage, displayMessageContext] = message.useMessage();
+
+    const onSubmitAttributeForm = async (data: IAttributeValue) => {
+        try {
+            console.log(data)
+            displayMessage.open({
+                type: 'success',
+                content: 'Success! Attribute added success'
+            });
+        } catch (error) {
+            displayMessage.open({
+                type: 'error',
+                content: 'Sorry! Attribute added failed'
+            });
+        }
+    };
+
     return (
         <Row>
             <Col xs={24} lg={12}>
+                {displayMessageContext}
                 <Title level={4}>Add New Attribute</Title>
-                <Form className='mt-4' layout='vertical'>
+                <Form
+                    className='mt-4'
+                    layout='vertical'
+                    onFinish={onSubmitAttributeForm}
+                >
                     <FormItem
                         label='Attribute Code'
                         name='code'
